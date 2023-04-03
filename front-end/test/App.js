bedcc303f7dff37a2afd1d5bce378f5818b55b39
Film-Choi <80976559+Film-Choi@users.noreply.github.com>
2023-04-03 14:37:56 +0900

    Edit login-page

--- front-end/test/App.js
+++ front-end/test/App.js
@@ -1,20 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const titleStyle = {
+    fontSize: 60,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 0,
+  };
+
+  const subtitleStyle = {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "normal",
+    textAlign: "center",
+  };
+  const subtitleStyle2 = {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "normal",
+    textAlign: "center",
+    marginBottom: 50,
+  };
+  const homeLogo = {
+    width: 200,
+    height: 200,
+  };
+  return (
+    <View style={styles.container}>
+      <Image
+        style={homeLogo}
+        source={require("./assets/crimson2positive.gif")}
+        resizeMode="contain"
+      />
+
+      <Text style={titleStyle}>KOREA</Text>
+      <Text style={subtitleStyle}>UNIVERSITY</Text>
+      <Text style={subtitleStyle2}>SEJONG CAMPUS</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={() => alert("Login")}>
+        <Text style={styles.buttonText}>로그인</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "brown",
+  },
+  Text: {
+    fontSize: 50,
+  },
+  input: {
+    width: "80%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "wheat",
+    backgroundColor: "white",
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "wheat",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
   },
 });
